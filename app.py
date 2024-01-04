d59706acbdca69df3ced55463c2b170e7ac10e8a
Harsha9198 <shriharsha.b@comakeit.com>
2024-01-04 16:13:13 +0530

    fixed all issues

--- app.py
+++ app.py
@@ -7,22 +7,23 @@ from langchain.cache import BaseCache,GPTCache
 from langchain.schema.prompt_template import BasePromptTemplate
 from langchain.prompts import PromptTemplate
 
-st.set_page_config(page_title="Documentation Assistant", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
-openai.api_key = "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"
-st.title("Chat with the CodeToCanvas docs💬")
-st.info("Get Back To Documentation [Documentation](https://tic.comakeit.com/)", icon="📃")
+st.set_page_config(page_title="Documentation Assistant", page_icon="🤖", layout="centered", initial_sidebar_state="auto", menu_items=None)
+openai.api_key = "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"
+st.title("Chat with the CanvasToCode docs💬")
+st.subheader("Talk to our canvas")
+st.warning("Get Back To site 👉 [CanvasTocode](https://tic.comakeit.com/)", icon="🌐")
          
 if "messages" not in st.session_state.keys(): # Initialize the chat messages history
     st.session_state.messages = [
-        {"role": "assistant", "content": "Ask me a question about CodetoCanvas!"}
+        {"role": "assistant", "content": "Ask me a question about CanvasToCode!"}
     ]
 
 @st.cache_resource(show_spinner=False)
 def load_data():
-    with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
+    with st.spinner(text="Loading and indexing the docs – hang tight! This should take 1-2 minutes."):
         reader = SimpleDirectoryReader(input_dir="./docs", recursive=True)
         docs = reader.load_data()
-        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features."))
+        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the CanvasToCode Documentation and your job is to answer user questions. Assume that all questions are related to the CanvasToCode Documentation. Keep your answers Explanatory and based on facts – do not hallucinate features."))
         index = VectorStoreIndex.from_documents(docs, service_context=service_context)
         return index
 
@@ -32,7 +33,7 @@ if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
         st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
 
 if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.session_state.messages.append({"role": "🧑🏻‍💻", "content": prompt})
 
 for message in st.session_state.messages: # Display the prior chat messages
     with st.chat_message(message["role"]):
@@ -40,9 +41,9 @@ for message in st.session_state.messages: # Display the prior chat messages
 
 # If last message is not from assistant, generate a new response
 if st.session_state.messages[-1]["role"] != "assistant":
-    with st.chat_message("assistant"):
+    with st.chat_message("🤖"):
         with st.spinner("Thinking..."):
             response = st.session_state.chat_engine.chat(prompt)
             st.write(response.response)
-            message = {"role": "assistant", "content": response.response}
+            message = {"role": "🤖", "content": response.response}
             st.session_state.messages.append(message) # Add response to message history
