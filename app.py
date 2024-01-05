37d65df7e0cc7d46895beae5a57948f3eb031e5b
Harsha9198 <shriharsha.b@comakeit.com>
2024-01-05 16:45:55 +0530

    changes going on

--- app.py
+++ app.py
@@ -3,19 +3,16 @@ from llama_index import VectorStoreIndex, ServiceContext, Document
 from llama_index.llms import OpenAI
 import openai
 from llama_index import SimpleDirectoryReader
-from langchain.cache import BaseCache,GPTCache
-from langchain.schema.prompt_template import BasePromptTemplate
-from langchain.prompts import PromptTemplate
 
 st.set_page_config(page_title="Documentation Assistant", page_icon="🤖", layout="centered", initial_sidebar_state="auto", menu_items=None)
-openai.api_key = "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"
-st.title("Chat with the CanvasToCode docs💬")
+openai.api_key = st.secrets.openai_key
+st.header("Chat with the CanvasToCode docs💬")
 st.subheader("Talk to our canvas")
 st.warning("Get Back To site 👉 [CanvasTocode](https://tic.comakeit.com/)", icon="🌐")
          
 if "messages" not in st.session_state.keys(): # Initialize the chat messages history
     st.session_state.messages = [
-        {"role": "🤖", "content": "Ask me a question about CanvasToCode!"}
+        {"role": "🤖", "content": "Ask me a question on how to get started with CanvasToCode!"}
     ]
 
 @st.cache_resource(show_spinner=False)
@@ -23,16 +20,16 @@ def load_data():
     with st.spinner(text="Loading and indexing the docs – hang tight! This should take 1-2 minutes."):
         reader = SimpleDirectoryReader(input_dir="./docs", recursive=True)
         docs = reader.load_data()
-        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the CanvasToCode Documentation and your job is to answer user questions. Assume that all questions are related to the CanvasToCode Documentation. Keep your answers Explanatory and based on facts – do not hallucinate features."))
+        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.7,system_prompt="You are a assistant for CodeToCanvas documentation and answer only when user gives a prompt, do not get queries of your own and donot give imaginary answers give only facts."))
         index = VectorStoreIndex.from_documents(docs, service_context=service_context)
         return index
 
 index = load_data()
 
 if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
-        st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
-
-if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
+        st.session_state.chat_engine = index.as_chat_engine( chat_mode="condense_question", verbose=True)
+prompt=st.chat_input("Your question")
+if prompt:# Prompt for user input and save to chat history
     st.session_state.messages.append({"role": "🧑🏻‍💻", "content": prompt})
 
 for message in st.session_state.messages: # Display the prior chat messages
